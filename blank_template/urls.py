04124cc8429db82041554eaba1c97a4cb45d5bb7
DomanskiJacek <DomanskiJacek@users.noreply.github.com>
2024-08-13 10:32:02 +0200

    edited in w3sch

--- blank_template/urls.py
+++ blank_template/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('blank.urls')),
+    path('testdictionary', include('aaa.urls')),
     path('admin/', admin.site.urls),
 ]
